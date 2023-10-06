fd1590ae7be583c66c259d659b0391c585ad6c23
Iker Guzman <104294300+Ik3rGuzm4n20@users.noreply.github.com>
2023-10-05 18:19:39 -0600

    Se agrego codigo y conexion a la base de datos.

---
+++ src/main/java/com/mycompany/formulario/Menu.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+package com.mycompany.formulario;
+
+import Interfaces.Formulario;
+import javax.swing.UIManager;
+
+/**
+ * 
+ * @author Sammy Guergachi <sguergachi at gmail.com>
+ */
+public class Menu {
+
+    public static void main(String[] args) {
+        Formulario vFor=new Formulario();
+    }
+}
